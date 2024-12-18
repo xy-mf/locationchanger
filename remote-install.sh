1868c73d14d5ee4213f2c310d9bbad14177a164c
yaoxiangyi <yao1909@163.com>
2024-12-18 13:00:33 +0800

    fix error

--- remote-install.sh
+++ remote-install.sh
@@ -6,18 +6,18 @@ LAUNCH_AGENTS_DIR=$HOME/Library/LaunchAgents
 PLIST_PATH=$LAUNCH_AGENTS_DIR/LocationChanger.plist
 CONFIG_DIR=$HOME/.locations
 
-RAW_SCRIPT_URL="https://github.com/xy-mf/locationchanger/raw/master/core"
+RAW_SCRIPT_URL="https://github.com/xy-mf/locationchanger/raw/master"
 
 sudo -v
 
 mkdir -p $CONFIG_DIR
-curl -sL $RAW_SCRIPT_URL/locations.conf.sample -o $CONFIG_DIR/locations.conf
+curl -sL $RAW_SCRIPT_URL/core/locations.conf.sample -o $CONFIG_DIR/locations.conf
 
 sudo mkdir -p $INSTALL_DIR
-sudo curl -sL $RAW_SCRIPT_URL/locationchanger.sh -o $SCRIPT_PATH
+sudo curl -sL $RAW_SCRIPT_URL/core/locationchanger.sh -o $SCRIPT_PATH
 sudo chmod +x $SCRIPT_PATH
 
 mkdir -p $LAUNCH_AGENTS_DIR
-curl -sL $RAW_SCRIPT_URL/LocationChanger.plist -o $PLIST_PATH
+curl -sL $RAW_SCRIPT_URL/core/LocationChanger.plist -o $PLIST_PATH
 launchctl load -w $PLIST_PATH
 
