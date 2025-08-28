d05dd4f5052ff51a326fde285944dbd11740cfe4
yaoxiangyi <yao1909@163.com>
2025-08-28 09:51:59 +0800

    fix: correct SSID detection for macOS 15.6

--- core/locationchanger.sh
+++ core/locationchanger.sh
@@ -16,6 +16,35 @@ ts "I am '$ID'"
 SSID=""
 NEW_LOCATION=""
 
+is-at-least() {
+    local version_a="$1"
+    local version_b="$2"
+
+    # 将版本号转换为可比较的数字格式
+    local IFS=.
+    local a=($version_a)
+    local b=($version_b)
+
+    # 填充缺失的版本位为0
+    while [[ ${#a[@]} -lt ${#b[@]} ]]; do
+        a+=(0)
+    done
+    while [[ ${#b[@]} -lt ${#a[@]} ]]; do
+        b+=(0)
+    done
+
+    # 逐位比较版本号
+    for ((i=0; i<${#a[@]}; i++)); do
+        if [[ ${a[i]} -lt ${b[i]} ]]; then
+            return 1  # 版本a小于版本b
+        elif [[ ${a[i]} -gt ${b[i]} ]]; then
+            return 0  # 版本a大于版本b
+        fi
+    done
+
+    return 0  # 版本相等
+}
+
 getSSID() {
     local wifi_port=$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}')
 
@@ -24,10 +53,21 @@ getSSID() {
         return 1
     fi
 
-    local mac_version=$(sw_vers -productVersion | cut -d '.' -f 1)
-    ts "macOS version is '$mac_version'"
+    local osVersion=$(sw_vers -productVersion)
+    ts "macOS version is '$osVersion'"
 
-    if [[ "$mac_version" -ge 15 ]]; then
+    # macOS 15.7 and later
+    if is-at-least "$osVersion" "15.7"; then
+        wirelessInterface=$(networksetup -listnetworkserviceorder | sed -En 's/^\(Hardware Port: (Wi-Fi|AirPort), Device: (en.)\)$/\2/p')
+        preferredWirelessNetworks=$(networksetup -listpreferredwirelessnetworks "${wirelessInterface}")
+        SSID=$(echo "$preferredWirelessNetworks" | head -1 | awk '{$1=$1;print}')  # 获取第一个首选网络
+
+    # macOS 15.6+
+    elif is-at-least "$osVersion" "15.6"; then
+        SSID=$(system_profiler SPAirPortDataType | awk '/Current Network Information:/ { getline; print substr($0, 13, (length($0) - 13)); exit }')
+
+    # macOS 15.0+
+    elif is-at-least "$osVersion" "15.0"; then
         SSID=$(ipconfig getsummary "$wifi_port" | awk -F ' SSID : ' '/ SSID : / {print $2}')
     else
         SSID=$(networksetup -getairportnetwork "$wifi_port" | awk '{print $NF}')
